b17d05a12911e6df438c3188a5d61167b5d844d9
Ikshit <ikshitgupta22@gmail.com>
2024-06-07 12:40:47 +0530

    finished team section

--- src/app/components/walnut_team.tsx
+++ src/app/components/walnut_team.tsx
@@ -5,13 +5,14 @@ const WalnutTeam = () => {
 	return (
 		<>
 			<div className="text-center text-4xl mt-10 font-bold text-headingColor max-md:mt-5 max-md:text-3xl"> Team </div>
-			<div className="flex gap-x-40 w-5/6 mx-auto items-center mt-5 max-md:gap-x-16">
-				<AspectRatio ratio={15 / 4} className="border-blue-900 border-4">
-					<Image src="/images/building.jpg" alt="Principal" blurDataURL="/images/building-small.jpg" placeholder="blur" fill />
+
+			<div className="flex gap-x-40 w-5/6 mt-5 max-md:gap-x-10 justify-center mx-auto md:px-32">
+				<AspectRatio ratio={9 / 4} className="border-blue-900 border-4">
+					<Image src="/images/team/principal.jpg" alt="Principal" blurDataURL="/images/team/principal-small.jpg" placeholder="blur" fill />
 				</AspectRatio>
 
-				<AspectRatio ratio={15 / 4} className="border-blue-900 border-4">
-					<Image src="/images/building.jpg" alt="Director" blurDataURL="/images/building-small.jpg" placeholder="blur" fill />
+				<AspectRatio ratio={9 / 4} className="border-blue-900 border-4">
+					<Image src="/images/team/director.jpg" alt="Director" blurDataURL="/images/team/director-small.jpg" placeholder="blur" fill />
 				</AspectRatio>
 			</div>
 		</>
