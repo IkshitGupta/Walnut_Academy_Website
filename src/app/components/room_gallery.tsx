880e65ce46579f749f094991178d31fd1e5919dc
Ikshit Gupta <ikshitgupta22@gmail.com>
2024-10-03 02:33:32 +0530

    Added blur images for room gallery

--- src/app/components/room_gallery.tsx
+++ src/app/components/room_gallery.tsx
@@ -4,11 +4,15 @@ import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import Image from "next/image";
 
 const RoomGallery = () => {
-	const images = [1, 2, 3, 4].map((num) => `/images/room/${num}.jpg`);
+	// Define both large and small image paths
+	const images = [1, 2, 3, 4, 5, 6, 7].map((num) => ({
+		large: `/images/room/${num}.jpg`,
+		small: `/images/room/small/${num}.jpg`, // Small placeholder image
+	}));
 
 	return (
 		<>
-			<div className="text-center text-4xl mt-10 font-bold text-headingColor max-md:mt-5 max-md:text-3xl"> Photo Gallery </div>
+			<div className="text-center text-4xl mt-10 font-bold text-headingColor max-md:mt-5 max-md:text-3xl">Photo Gallery</div>
 
 			<div className="mt-5 w-4/5 mx-auto max-md:w-4/6">
 				<Carousel
@@ -18,10 +22,19 @@ const RoomGallery = () => {
 						loop: true,
 					}}>
 					<CarouselContent className="-ml-10">
-						{images.map((src, index) => (
+						{images.map((image, index) => (
 							<CarouselItem className="basis-1/3 pl-10 max-md:basis-1/2" key={index + 1}>
 								<AspectRatio ratio={1 / 1} className="border-blue-900 border-4">
-									<Image src={src} alt={`Room ${index + 1} - A snapshot of one of the rooms at Walnut Academy`} blurDataURL="/images/building-small.jpg" placeholder="blur" fill sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw" />
+									<Image
+										src={image.large}
+										alt={`Room ${index + 1} - A snapshot of one of the rooms at Walnut Academy`}
+										blurDataURL={image.small} // Use small image for placeholder blur
+										placeholder="blur"
+										fill
+										sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+										loading="lazy"
+										className="object-cover"
+									/>
 								</AspectRatio>
 							</CarouselItem>
 						))}
