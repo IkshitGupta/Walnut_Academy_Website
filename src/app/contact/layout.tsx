116a7c56fd2e35c6a06124396f5e2b4501c712ef
Ikshit Gupta <ikshitgupta22@gmail.com>
2024-10-07 00:31:10 +0530

    Changed icon

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,9 +1,13 @@
 export const metadata = {
 	title: "Contact Information",
 	description: "Get in touch with Walnut Academy for inquiries, admissions, and more.",
-	keywords: ["Walnut Academy", "Contact", "School", "Mansarovar", "Jaipur", "PG", "KG", "Nursery", "Prep", "Pre School, English Medium"],
+	keywords: ["Walnut Academy", "Contact", "School", "Mansarovar", "Jaipur", "PG", "KG", "Nursery", "Prep", "Pre School", "English Medium"],
 	robots: "index, follow", // Allow search engines to index the page
-	viewport: "width=device-width, initial-scale=1.0",
+};
+
+export const viewport = {
+	width: "device-width",
+	initialScale: 1.0,
 };
 
 export default function ContactLayout({ children }: { children: React.ReactNode }) {
