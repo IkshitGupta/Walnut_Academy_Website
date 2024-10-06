173fc237c6582e92244b9e0df4bb57c858d6b77f
Ikshit Gupta <ikshitgupta22@gmail.com>
2024-10-07 00:03:17 +0530

    Added meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="Walnut Academy - A premier educational institution in Mansarovar, Jaipur." />
+				<meta name="keywords" content="Walnut Academy, School, Education, Jaipur, Mansarovar, PreSchool, Nursery, KG, Prep, Play Group" />
 				<title>Walnut Academy</title>
+				{/* Open Graph Meta Tags */}
+				<meta property="og:title" content="Walnut Academy" />
+				<meta property="og:description" content="Walnut Academy - A premier educational institution in Mansarovar, Jaipur." />
+				<meta property="og:image" content="/images/building-crop.jpg" /> {/* Replace with your actual image path */}
+				<meta property="og:url" content="https://yourwebsite.com" />
+				<meta property="og:type" content="website" />
 			</head>
 			<body className="box-border m-0 p-0">
 				{/* Header Section */}
